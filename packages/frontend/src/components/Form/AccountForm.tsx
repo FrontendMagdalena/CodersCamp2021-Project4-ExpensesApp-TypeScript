5876d65bc09b8848dc606c40ee43db0db57e9fb9
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-04-23 19:25:13 +0200

    Revert "remove grid-templat-rows on new position view"
    This reverts commit 8a6fbe0a2e6bd19d6f04d102fec26e2199ab0f7f.

--- packages/frontend/src/components/Form/AccountForm.tsx
+++ packages/frontend/src/components/Form/AccountForm.tsx
@@ -6,9 +6,9 @@ import {
   InputGroupStyled,
 } from '../../views/NewPosition/NewPosition.styles';
 import { Input, InputSelect, InputAttachment } from '../Input/Input';
-import { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import {ChangeEvent, useCallback, useEffect, useState} from 'react';
 import { apiUrl } from '../../utils/serverURL';
-import { IAccount } from '../../reducers/accounts.reducer';
+import {IAccount} from "../../reducers/accounts.reducer";
 
 const colors = [
   '#F4600C',
@@ -27,17 +27,17 @@ const getRandomInt = (min: number, max: number) => {
 const token = JSON.parse(localStorage.getItem('user') as string)?.token;
 
 interface IAccountFormData {
-  amount: string | null;
-  category: { value: string; label: string };
-  title: string;
-  date: string;
-  type: 'Wydatek' | 'Przychód';
+  amount: string | null,
+  category: { value: string, label: string},
+  title: string,
+  date: string,
+  type: 'Wydatek' | 'Przychód',
 }
 
 interface IProps {
-  handleSubmit: (data: IAccountFormData) => void;
-  account?: IAccount;
-  buttonText: string;
+  handleSubmit: (data: IAccountFormData) => void
+  account?: IAccount,
+  buttonText: string
 }
 
 export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
@@ -50,7 +50,7 @@ export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
       },
     });
     const data = await response.json();
-    const translatedData = data.map((item: { name: string }) => {
+    const translatedData = data.map((item: {name: string}) => {
       return {
         ...item,
         label: item.name,
@@ -58,17 +58,15 @@ export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
       };
     });
     const initialCategory = translatedData.find(
-      (item: { label: string }) => item.label === account?.category,
+      (item: {label: string}) => item.label === account?.category,
     );
     initialCategory && setCategory(initialCategory);
     setCategoryList(translatedData);
   };
 
-  const [categoryList, setCategoryList] = useState<
-    { value: string; label: string; color: string }[]
-  >([]);
+  const [categoryList, setCategoryList] = useState<{ value: string, label: string, color: string }[]>([]);
 
-  const addNewCategory = async (data: { name: string; color: string }) => {
+  const addNewCategory = async (data: {name: string, color: string, } ) => {
     const userID = JSON.parse(localStorage.getItem('user') as string).id;
     const response = await fetch(apiUrl.categories, {
       method: 'POST',
@@ -91,13 +89,8 @@ export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
     : new Date().toISOString().slice(0, 10);
 
   const [date, setDate] = useState<string>(formattedDate);
-  const [type, setType] = useState<'Wydatek' | 'Przychód'>(
-    account?.type || 'Wydatek',
-  );
-  const [category, setCategory] = useState<{ label: string; value: string }>({
-    label: '',
-    value: '',
-  });
+  const [type, setType] = useState<'Wydatek' | 'Przychód'>(account?.type || 'Wydatek');
+  const [category, setCategory] = useState<{ label: string, value: string }>({ label: '', value: '' });
   const [title, setTitle] = useState<string>(account?.title || '');
   // TODO think how to remove console warning
   const [amount, setAmount] = useState<string | null>(account?.amount || null);
@@ -105,7 +98,7 @@ export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
 
   const handleCreate = useCallback(
     (inputValue) => {
-      const newValue: { value: string; label: string; color: string } = {
+      const newValue: { value: string, label: string, color: string } = {
         value: inputValue.toLowerCase(),
         label: inputValue,
         color: colors[getRandomInt(0, colors.length)],
@@ -142,9 +135,7 @@ export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
             type="date"
             inputLabel="Data"
             value={date}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setDate(e.target.value)
-            }
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setDate(e.target.value)}
           />
         </InputDateStyled>
         <ExpIncBtnGroup>
@@ -165,26 +156,20 @@ export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
             inputLabel="Kategoria"
             value={category}
             options={categoryList}
-            onChange={(option: { label: string; value: string }) =>
-              setCategory(option)
-            }
+            onChange={(option: { label: string, value: string}) => setCategory(option)}
             onCreateOption={handleCreate}
           />
           <Input
             type="text"
             inputLabel="Nazwa"
             value={title}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setTitle(e.target.value)
-            }
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
           />
           <Input
             type="number"
             inputLabel="Kwota"
             value={amount}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setAmount(e.target.value)
-            }
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setAmount(e.target.value)}
           />
           <InputAttachment
             type="file"
@@ -203,3 +188,4 @@ export const AccountForm = ({ handleSubmit, account, buttonText }: IProps) => {
     </>
   );
 };
+
