a6aac9001df1e783150e34190c89aa1ccb2781c3
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-04-24 21:06:15 +0200

    change url for backend

--- packages/frontend/src/utils/serverURL.ts
+++ packages/frontend/src/utils/serverURL.ts
@@ -1,7 +1,7 @@
 export const serverURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://scrooge-f6e34.ondigitalocean.app:8080';
+    : 'https://scrooge-back-qd7rl.ondigitalocean.app';
 console.log(`${process.env.NODE_ENV}`);
 console.log(`Server URL: ${serverURL}`);
 
